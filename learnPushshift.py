0e030653f8ddc2a214c2411dd2ddd5278dbd715f
Matthew Ho <msho@ucsb.edu>
2021-10-28 23:39:53 -0700

    pivoting to questions

--- learnPushshift.py
+++ learnPushshift.py
@@ -3,6 +3,7 @@ import datetime
 import requests
 import json
 import csv
+import datetime
 
 """
 # datetime
@@ -17,36 +18,73 @@ print('response: ', response1)
 """
 
 # pushshift request
-def addUrlParam(url, name, val):
+def add_url_param(url, name, val):
 	if not val:
 		url += ('&' + name + '=' + val)
 	return url
 
-def buildURL(after = None, before = None, search = None, sub = None):
-	url = 'https://api.pushshift.io/reddit/search/submission/?'
-	params = {'after':after, 'before':before, 'search':search, 'subreddit':sub}
-	firstParam = True
+def build_url(after=None, before=None, search=None, sub=None, size=None):
+	url = 'https://api.pushshift.io/reddit/search/submission/?sort_type=created_utc'
+	params = {
+		'after':after, 
+		'before':before, 
+		'search':search, 
+		'subreddit':sub,
+		'size':size
+	}
 	for name in params:
 		if params[name] is not None:
-			# if not firstParam:
-			if firstParam:
-				firstParam = False
-			else:
-				url += '&'
+			url += '&'
 			url += (name + '=' + str(params[name]))
 	return url
 
-def getPosts(after = None, before = None, search = None, sub = None):
+def get_posts(after=None, before=None, search=None, sub=None, size=None):
 	# return None
-	url = buildURL(after, before, search, sub)
-	print('url: ', url)
+	url = build_url(after, before, search, sub, size)
+	# print('url: ', url)
 	r = requests.get(url)
 	data = json.loads(r.text)
 	return data['data']
 
 # print(buildURL(after = datetime.datetime.now().timestamp(), sub = 'dogecoin'))
 # print(getPosts(after = '1d', sub = 'dogecoin'))
-prevHour = getPosts(after = '1h', sub = 'dogecoin')
-print('len: ', len(prevHour))
-print('item0: ', prevHour[0])
 
+# prevHour = getPosts(after='1d', sub='nostupidquestions')
+# print('len: ', len(prevHour))
+# print('item0: ', prevHour[0])
+# 
+# ct = datetime.datetime.now()
+# after_t = ct - datetime.timedelta(days=1)
+# ts = str(after_t.timestamp()).split('.')[0]
+# 
+# print('timestamp:', ts)
+# prevHour2 = getPosts(after=ts, sub='nostupidquestions')
+# print('len: ', len(prevHour2))
+# print('item0: ', prevHour2[0])
+
+# print('they are equal?', prevHour == prevHour2)
+
+SIZE_LIMIT = 100
+def get_x_posts(x, data, search=None, sub=None, before=None):
+	if x <= SIZE_LIMIT:
+		data.extend(get_posts(search=search, sub=sub, size=x))
+	else:
+		next_batch = get_posts(search=search, sub=sub, size=SIZE_LIMIT)
+		# print('current batch size:', len(next_batch))
+		next_earliest = next_batch[0]['created_utc']
+		data.extend(next_batch)
+		get_x_posts(x - SIZE_LIMIT, data, search=search, sub=sub, before=next_earliest)
+
+def is_why_q(post):
+	# return (post['title'][0:3].lower() == 'why')
+	return ('why' in post['title'].lower())
+
+def get_why_qs(sub, num_posts):
+	posts = []
+	get_x_posts(num_posts, posts, sub=sub)
+	return [post['title'] for post in posts if is_why_q(post)]
+
+why_qs = get_why_qs('askreddit', 500)
+print(len(why_qs))
+if len(why_qs) > 10:
+	print(why_qs[:10])
